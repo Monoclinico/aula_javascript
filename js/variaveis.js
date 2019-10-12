116003b0c11c0568f3b543d6d46b300cd301221c
Matheus Sanches <matheussanchesleme@gmail.com>
2019-10-12 13:58:02 -0300

    tipos de variáveis no JS

---
+++ js/variaveis.js
@@ -0,0 +1,7 @@
+var nomev = "Matheus";
+let nomel = "Matheus";
+const nomec = "Matheus";
+
+console.log("Nome da variavel: "+nomev);
+console.log("Nome da variavel: "+nomel);
+console.log("Nome da variavel: "+nomec);
